ff90fc1eaec9c856f1c9d669de33a979fdf864f7
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-05-20 16:00:20 +0530

    nft-project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x4590CDC4e361c5D84B607136ddE99e263edFb786";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
